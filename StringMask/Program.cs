ea7e031ec33bf0675b3190a946da3a105d2741ce
milian <liaoyalien@gmail.com>
2019-09-16 23:30:19 +0800

    string mask

---
+++ StringMask/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace StringMask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var origin = "廖小米";
+            var masked = origin.ToMask('*');
+        }
+
+    
+    }
+}
---
+++ StringMask/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringMask
+{
+    public static class StringExtension
+    {
+        //遮住字首以後的其餘字元
+        public static string ToMask(this string str, char maskcharacter)
+        {
+            return $"{str.Substring(0, 1)}{string.Concat(Enumerable.Repeat(maskcharacter, str.Length - 1))}";
+
+        }
+    }
+}
